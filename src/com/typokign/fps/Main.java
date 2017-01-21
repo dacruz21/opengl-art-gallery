41911cd1d9cbfb804546f00d7bde662afff9da72
Typo Kign <typokign1@gmail.com>
2017-01-21 13:25:06 -0800

    Initial commit w/ Hello World

---
+++ src/com/typokign/fps/Main.java
@@ -0,0 +1,10 @@
+package com.typokign.fps;
+
+/**
+ * Created by Typo Kign on 1/21/2017.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
